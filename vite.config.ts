66cf5e3107f877b15a0464d4b5a9c08e67c6cb44
Z M <246810zoom@gmail.com>
2026-03-06 02:12:54 +1100

    g

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
@@ -18,5 +18,8 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
     dedupe: ["react", "react-dom"],
+  },
+  build: {
+    minify: "esbuild",
   },
 }));
