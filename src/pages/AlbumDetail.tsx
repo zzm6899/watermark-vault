ed2206a2a53fa8527803fd186e8c198981d7fb7e
Z M <246810zoom@gmail.com>
2026-03-05 16:57:00 +1100

    g

--- src/pages/AlbumDetail.tsx
+++ src/pages/AlbumDetail.tsx
@@ -14,6 +14,7 @@ import { resizeToTargetSize } from "@/lib/image-utils";
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
